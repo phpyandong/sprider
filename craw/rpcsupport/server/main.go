f5c8df5e61830d869ceebeaafd240cb6be55207b
phpyandong <seiyaz@163.comseiyaz@163.comseiyaz@163.comseiyaz@163.com>
2021-02-01 17:15:57 +0800

    http服务的 信号绑定kill -9 ; 设置httpurl 的超时时间，超时关闭服务

--- craw/rpcsupport/server/main.go
+++ craw/rpcsupport/server/main.go
@@ -5,10 +5,16 @@ import (
 	"sprider/craw/store"
 	"gopkg.in/olivere/elastic.v5"
 	"log"
-	"sprider/craw/config"
-	"fmt"
-	"sprider/redis"
 	"flag"
+	"os/signal"
+	"syscall"
+	"os"
+	"net/http"
+	"time"
+	"context"
+	"sprider/craw/config"
+	"sprider/redis"
+	"fmt"
 )
 
 var port = flag.Int("port", 0,
@@ -34,6 +40,8 @@ func main()  {
 	host := fmt.Sprintf(":%d",*port)
 	//初始化redis链接池
 	redis.InitRedis()
+
+
 	//time.Sleep(time.Second*5)
 	//redis.GetRedis().Get()
 	//time.Sleep(time.Second*5)
@@ -47,8 +55,48 @@ func main()  {
 	//log.Fatal(serverRpc(host,"data_profile"))
 	log.Fatal(ServerGRpc(host,"data_profile"))
 
+	//go HttpServer()
 
 
+}
+func HttpServer(){
+	server := &http.Server{
+		Addr:    ":8080",
+	}
+	http.HandleFunc("/ping", func(res http.ResponseWriter, req *http.Request){
+		_, err := res.Write([]byte("pong"));
+		if err != nil{
+			log.Fatal("write err--->",err)
+		}
+	})
+	server.ListenAndServe()
+	// 用于捕获退出信号
+	quit := make(chan os.Signal)
+
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// 设置超时时间，两个心跳周期，假设一次心跳 3s
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancelFunc()
+
+	// Shutdown 接口，如果没有新的连接了就会释放，传入超时 context
+	// 调用这个接口会关闭服务，但是不会中断活动连接
+	// 首先会将端口监听移除
+	// 然后会关闭所有的空闲连接
+	// 然后等待活动的连接变为空闲后关闭
+	// 如果等待时间超过了传入的 context 的超时时间，就会强制退出
+	// 调用这个接口 server 监听端口会返回 ErrServerClosed 错误
+	// 注意，这个接口不会关闭和等待websocket这种被劫持的链接，如果做一些处理。可以使用 RegisterOnShutdown 注册一些清理的方法
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
 func ServerGRpc(host,index string) error{
 	client, err := elastic.NewClient(

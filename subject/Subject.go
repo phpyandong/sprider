1facedca174c7125ed30da0c8e1cd75dfcd07dc7
phpyandong <seiyaz@163.comseiyaz@163.comseiyaz@163.comseiyaz@163.com>
2021-02-04 10:44:08 +0800

    子集算法

--- subject/Subject.go
+++ subject/Subject.go
@@ -4,7 +4,7 @@ import (
 	"math/rand"
 	"fmt"
 )
-//todo 算法背景 由于实现的是客户端心跳检测，过多的客户端检测服务端，服务端压力很大，因此需要从全部的节点取一部分，
+//todo 子集算法背景 由于实现的是客户端心跳检测，过多的客户端检测服务端，服务端压力很大，因此需要从全部的节点取一部分，
 // from google sre
 //为什么上面这个算法可以保证可以均匀分布？
 //首先，shuffle 算法保证在 round 一致的情况下，backend 的排列一定是一致的。
@@ -12,8 +12,13 @@ import (
 // 因此在同一个 round 内的 client 会分别 backend 排列的不同部分的切片作为选中的后端服务来建立连接。
 //所以只要 client id 是连续的，那么 client 发向 后端的连接就一定是连续的
 //参考资料:
+/**
+	backends 后端节点
+	clientId 消费者Id
+	subsetSize 子集大小
+ */
 func Subset(backends []string, clientID, subsetSize int) []string {
-	subsetCount := len(backends) / subsetSize
+	subsetCount := len(backends) / subsetSize //几批子集
 	fmt.Println("subsetcount:11/5",subsetCount)
 	// Group clients into rounds; each round uses the same shuffled list:
 	//对客户端 进行分组;每一轮使用相同的洗牌列表:

ae407c25e01056ad5a468f4404bc28337d289ce4
phpyandong <seiyaz@163.com>
2021-02-19 16:47:23 +0800

    读取配置及初始化redis的最佳实践，代码kratos待发布

--- craw/configs/config_test.go
+++ craw/configs/config_test.go
@@ -1,7 +1,35 @@
 package configs
 
-import "testing"
+import (
+	"testing"
+	"github.com/bilibili/kratos/pkg/cache/redis"
+	"github.com/ghodss/yaml"
+	"common"
+)
 
 func TestInit(t *testing.T) {
 	Init()
+
+}
+//伪代码
+//读取yaml文件，并赋值给redis config struct
+func ApplyYAML(s *redis.Config,yml string) error{
+	js , err := yaml.YAMLToJSON([]byte(yml))
+	if err != nil {
+		return err
+	}
+	return ApplyJSON(s,string(js))
+}
+func Options(c *redis.Config) []redis.Options{
+	return []redis.Options{
+		redis.DialDatabase(c.Database),
+		redis.DialPassword(c.Password),
+		redis.DialReadTimeout(c.ReadTimeout),
+	}
+}
+func TestRedisConf(t *testing.T) {
+
+	c := new (redis.Config)
+	_= ApplyYAML(c,common.LoadConfig())
+	r ,_:= redis.Dial(c.NetWork,c.Address,Options(c))
 }

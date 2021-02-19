35b4d6d340837b7a7c0cc853c38e3ef8f2dcb413
phpyandong <seiyaz@163.com>
2021-02-19 19:09:30 +0800

    add

--- craw/configs/config_test.go
+++ craw/configs/config_test.go
@@ -11,7 +11,8 @@ func TestInit(t *testing.T) {
 	Init()
 
 }
-//伪代码
+//=========================================
+//伪代码 最佳实践
 //读取yaml文件，并赋值给redis config struct
 func ApplyYAML(s *redis.Config,yml string) error{
 	js , err := yaml.YAMLToJSON([]byte(yml))
@@ -20,6 +21,11 @@ func ApplyYAML(s *redis.Config,yml string) error{
 	}
 	return ApplyJSON(s,string(js))
 }
+//package redis
+type Option interface{
+	apply(*options)
+}
+
 func Options(c *redis.Config) []redis.Options{
 	return []redis.Options{
 		redis.DialDatabase(c.Database),
@@ -30,6 +36,6 @@ func Options(c *redis.Config) []redis.Options{
 func TestRedisConf(t *testing.T) {
 
 	c := new (redis.Config)
-	_= ApplyYAML(c,common.LoadConfig())
+	_= ApplyYAML(c,LoadConfig())
 	r ,_:= redis.Dial(c.NetWork,c.Address,Options(c))
 }

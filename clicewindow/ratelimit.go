e8bf5babc2fca0748ee932e2d62cee1555f99d77
phpyandong <seiyaz@163.com>
2021-02-20 18:23:05 +0800

    redis滑动窗口处理

--- clicewindow/ratelimit.go
+++ clicewindow/ratelimit.go
@@ -1,14 +1,28 @@
 package clicewindow
 
 import (
-"container/ring"
-"errors"
-"fmt"
-"math"
-"sync"
-"time"
+	"sync"
+	"container/ring"
+	"math"
+	"time"
 )
 
+/**
+redis 滑动窗口实现
+public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) {
+	String key = String.format("hist:%s:%s", userId, actionKey);
+	long nowTs = System.currentTimeMillis();
+	Pipeline pipe = jedis.pipelined();
+	pipe.multi();
+	pipe.zadd(key, nowTs, "" + nowTs);
+	pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+	Response<Long> count = pipe.zcard(key);
+	pipe.expire(key, period + 1);
+	pipe.exec();
+	pipe.close();
+	return count.get() <= maxCount;
+}
+**/
 // 窗口节点
 type bucketNode struct {
 	startTime int64 // 节点开始时间
